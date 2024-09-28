3cf47ad9b941f110a5c9b076ed2310073d54826e
ljihyun99 <Ji.Lee@campus.lmu.de>
2024-09-28 22:08:22 +0200

    final

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,17 +1,18 @@
 import { useParams } from 'react-router-dom';
 import FilmsData from '../Films';
+import { Link } from 'react-router-dom';
 
 const Details = ()=> {
     const {id} = useParams();
-    console.log(id);
     const movies = FilmsData;
     const movie = movies.filter((movie)=> movie.imdbID === id);
-    console.log(movie[0]);
+
     return(
         <div>
             <p>Genre: {movie[0].Genre}</p>
             <p>Actors: {movie[0].Actors}</p>
             <p>Plot: {movie[0].Plot}</p>
+            <Link to="/"><button>Go back</button></Link>
         </div>
     )
 };
